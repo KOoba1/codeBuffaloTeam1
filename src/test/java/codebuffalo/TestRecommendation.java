9f6b2a2cfec6da97a6b2b21f52762e73c0a5953d
brassogre <brassogre@gmail.com>
2019-06-15 17:19:51 -0400

    Added unit tests

--- src/test/java/codebuffalo/TestRecommendation.java
+++ src/test/java/codebuffalo/TestRecommendation.java
@@ -24,8 +24,38 @@ public class TestRecommendation {
     }
 
     @Test
-    public void testEvents() {
-//        List<Event> events = Recommendation.events();
-//        Assert.assertTrue(2, events.size);
+    public void testEvents1() {
+        List<Event> events = Recommendation.events(5490351);
+        Assert.assertEquals("Should be 3 events", 3, events.size());
+    }
+
+    @Test
+    public void testEvents2() {
+        List<Event> events = Recommendation.events(2211716);
+        Assert.assertEquals("Should be 2 events", 2, events.size());
+    }
+
+    @Test
+    public void testEvents3() {
+        List<Event> events = Recommendation.events(3192099);
+        Assert.assertEquals("Should be 1 event", 1, events.size());
+    }
+
+    @Test
+    public void testEvents4() {
+        List<Event> events = Recommendation.events(9216391);
+        Assert.assertEquals("Should be 1 event", 1, events.size());
+    }
+
+    @Test
+    public void testEvents5() {
+        List<Event> events = Recommendation.events(6509779);
+        Assert.assertEquals("Should be 1 event", 1, events.size());
+    }
+
+    @Test
+    public void testEvents6() {
+        List<Event> events = Recommendation.events(3334444);
+        Assert.assertEquals("Should be 0 events", 0, events.size());
     }
 }
