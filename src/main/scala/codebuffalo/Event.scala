c5ddd1a6e44c7eafec2fab5dd07d4d8ac904ad05
brassogre <brassogre@gmail.com>
2019-06-15 17:10:18 -0400

    Created final data objects

--- src/main/scala/codebuffalo/Event.scala
+++ src/main/scala/codebuffalo/Event.scala
@@ -10,15 +10,12 @@ final case class Event(eventId: Int,
                        price: Double,
                        time: String)
 
-final case class Activity(key: Int,
-                       activityId: Int,
-                       name: String,
-                       description: String,
-                       address: String,
-                       category: String,
-                       popularity: Double,
-                       price: Double,
-                       time: String)
+final case class Activity(activity: String,
+                          accessibility: Double,
+                          `type`: String,
+                          participants: Int,
+                          price: Double,
+                          key: String)
 
 final case class Query(activityId: Int,
                        activity: Activity,
