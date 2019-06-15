730324d095a0dfe09c5632084e7c0b93cd5dcdb4
AnasHamed73 <anas.ta.hamed@gmail.com>
2019-06-15 14:13:38 -0400

    first version of graphQL integration

---
+++ src/main/java/codebuffalo/GraphQLTest.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
+public class GraphQLTest {
+
+    public static void main(String[] args) throws Exception {
+//        String schema = "type Query{hello: String} type schema{query: Query}";
+        String schema = "type Query{hello: String} type schema{query: Query}";
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+        RuntimeWiring runtimeWiring = newRuntimeWiring()
+//                .type("Query",
+//                        builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+                .type("Query",
+                        builder -> builder.dataFetcher("hello",
+                                e -> sendGet("https://www.boredapi.com/api/activity?particiants=1")))
+                .build();
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = build.execute("{hello}");
+
+        System.out.println(executionResult.getData().toString());
+        // Prints: {hello=world}/
+
+//        String resp = sendGet("https://www.boredapi.com/api/activity?particiants=1");
+    }
+
+    private static String sendGet(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        int responseCode = con.getResponseCode();
+        StringBuffer response = new StringBuffer();
+        try(BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+        }
+        return response.toString();
+    }
+}
